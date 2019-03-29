ca2ded7ccba0edd11caf3991a0e6c8866ce88afc
Mohit Soni <mohit_soni87@ymail.com>
2019-03-30 02:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,304 +4,311 @@ var app = express();
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
-
 const joi = require('joi');
-status = 0        //to check active logins
-
-//Offers Function
-
-function checkOffers(price) {
-    if(price >= 1500 && price < 2000){
-        var tax = 0.18*price;
-        var discountedprice = price - 0.15*price + tax;
-        return  discountedprice
-    }
-    else if(price >= 2000){
-        var tax = 0.18*price;
-        var discountedprice = price - 0.2*price + tax;
-        return discountedprice
-    }
-    
- } 
-
-//database
-Games = {'EA': ['Fifa 18', 'Cricket 17'],
-        'Ubisoft': ['FarCry New Dawn', "Assassin's Creed Origins", "Assassin's Creed Syndicate", "Assassin's Creed II"]};
-
-gameDetail = {'Fifa 18': [{'Price': 1800}, {'Studio': 'EA'}, {'Tax': 18}, {'Stock': 0}], 
-              'Cricket 17': [{'Price': 800}, {'Studio': 'EA'}, {'Tax': 18}, {'Stock': 1}],
-              'FarCry New Dawn': [{'Price': 2000}, {'Studio': 'Ubisoft'}, {'Tax': 18}, {'Stock': 2}],
-              "Assassin's Creed Origins": [{'Price': 2200}, {'Studio': 'Ubisoft'}, {'Tax': 18}, {'Stock': 5}],
-              "Assassin's Creed Syndicate": [{'Price': 1700}, {'Studio': 'Ubisoft'}, {'Tax': 18}, {'Stock': 1}],
-              "Assassin's Creed II": [{'Price': 900}, {'Studio': 'Ubisoft'}, {'Tax': 18}, {'Stock': 3}],
-            }
 
 
-var Cart = {}
-var CartValue = {}
-User = {}
-var Users = {}
-var enrolled = {}
-var Order = {}
+
+Category = {
+    'Sports': ['Fifa 18', 'Cricket 17'],
+    'Action': ['FarCry New Dawn', "Assassin's Creed Origins", "Assassin's Creed Origins", "Assassin's Creed Syndicate"],
+    'Adventure': ['FarCry New Dawn', "Assassin's Creed Origins", "Assassin's Creed Origins", "Assassin's Creed Syndicate"]
+}
+
+Brand = {'EA': ['Fifa 18', 'Cricket 17'],
+        'Ubisoft': ['FarCry New Dawn', "Assassin's Creed Origins", "Assassin's Creed Syndicate", "Assassin's Creed II", "Assassin's Creed II"],
+        }
 
 
-//Registration MiddleWare  
-app.use('/registeruser', function(req, res, next){
-    console.log('re');
-    User = req.query;
-    if(User.Username in Users){
-       res.json("Username already exists.");     // Handling Validations 
-    }
-    else{
- 
-       //JOI VALIDATION
-       const schema = {
-        Username: joi.string().min(5).max(9).required(),        //Username should be in-between [5, 7]
-       }
- 
-       const result = joi.validate({'Username': req.query.Username}, schema);
-       
-       if (result.error != null && result.error.details[0].message) {
-          res.status(400).send(result.error.details[0].message);
-          return;
+
+gameDetail = {'Fifa 18': [{'Price': 1800}, {'Creator': 'EA'}, {'Tax': 18}], 
+        'Cricket 17': [{'Price': 800}, {'Creator': 'EA'}, {'Tax': 18}],
+        'FarCry New Dawn': [{'Price': 2000}, {'Creator': 'Ubisoft'}, {'Tax': 18}],
+        "Assassin's Creed Origins": [{'Price': 2200}, {'Creator': 'Ubisoft'}, {'Tax': 18}],
+        "Assassin's Creed Syndicate": [{'Price': 1700}, {'Creator': 'Ubisoft'}, {'Tax': 18}],
+        "Assassin's Creed II": [{'Price': 900}, {'Creator': 'Ubisoft'}, {'Tax': 18}],
       }
-       else{
-          Users[req.query.Username] = req.query.Password;
-          status = 1;
-          next();
-       }
-    }
- 
- }  
- );
- 
+  
+//Add GameDetail
+function AddToGameDetail(gameName, Price, Creator){
+    gameDetail[gameName] = [{'Price': Price}, {'Creator': Creator}, {'Tax': 18}]
+} 
 
 
- //Login MiddleWare     
+//Add game into studio object
 
-app.use('/login', function(req, res, next){
-
-    if(req.query.Username in Users){
-       if( Users[req.query.Username] == req.query.Password){
-          status = 1;
-          User = req.query;
-          res.redirect('/');
-          next();
-          
-       }
-       else{
-          res.json("Invalid Credentials.");         //  Handling Validations 
-       }
- 
- }
- else{
-    res.json(req.query.Username.toString() + " username doesn't exist.");    //  Handling Validations 
- }
- 
- })
- 
-
-//Registration
-app.post('/registeruser', function(req, res){
-    User = req.query;
-    res.json("Successfully registered!");
- });
-
-//Login 
-app.get('/login', function(req, res){
-    
- });
- 
-
-//Logout
-app.get('/logout', function(req, res){
-
-    if(!status){
-        res.json("You are not logged in!");
+function AddToBrand(brand, gameName){
+    if(brand in Brand){
+        Brand[brand].push(gameName);
     }
     else{
-        status = 0;
-        User = {}
-        console.log("logged off!");
-        res.redirect("/");
+        Brand[brand] = [gameName];
     }
-
- });
- 
+} 
 
 
-
- app.get('/url', function(req, res){
-    res.json("Welcome to E-Kart!");
- });
-
-//Homepage
-app.get('/', function(req, res){
-    if(Object.keys(User).length >= 1){
-        res.json("Hello! " + User.Username + ". Welcome to E-Kart!");
-    }
-    else{
-        res.json("Welcome to E-Kart! Please log in to enjoy the services.");
-    }
-    
-});
-
-//get all available games
-app.get('/getall', function(req, resp){
-    resp.json(Games);
-
-});
-
-//Specific Game Detail
-app.post('/gamedetail/', function(req, resp){
-    
-    resp.json(gameDetail[req.query.GameName]);
-});
-
-//Add to Cart
-app.post('/addtoCart', function(req, resp){
-    if(!status){
-        resp.json("Please log in first.");  //if the user is not logged in.
-    }   
-    else{
-        if(User.Username in Cart ){
-
-            //Checking the stock
-            console.log(gameDetail[(req.query.GameName)][3].Stock);
-            if(gameDetail[(req.query.GameName)][3].Stock >= 1){
-                Cart[User.Username].push(req.query.GameName);
-                CartValue[User.Username] += gameDetail[(req.query.GameName)][0].Price;
-                
+//Deleting Game from Category
+function deleteFromCategory(gameName){
+    Object.keys(Category).forEach(key => {
+        console.log(key)
+        var value = Category[key];
+        var i = 0;
+        for(i = 0; i < value.length; i++){
+            console.log(value[i]);
+            if(value[i] == gameName){
+                Category[key].splice(i, 1);
+                console.log("break");
+                break;
             }
-            else{
-                resp.json("We're Sorry! " + req.query.GameName + " is currently not available. We have noted the product and will add it soon to the stock! Thank you!");
+        }
+      });  
+} 
+
+
+//Deleting Game Array from Category
+function deleteArrayFromCategory(gameName){
+    var j = 0;
+    for(j = 0; j < gameName.length; j++){
+        Object.keys(Category).forEach(key => {
+            console.log(key)
+            var value = Category[key];
+            var i = 0;
+            for(i = 0; i < value.length; i++){
+                console.log(value[i]);
+                if(value[i] == gameName[j]){
+                    Category[key].splice(i, 1);
+                    break;
+                }
             }
-
-            
-        }
-        else{
-            Cart[User.Username] = [(req.query.GameName)];
-            CartValue[User.Username] = gameDetail[(req.query.GameName)][0].Price;
-        }
-        resp.json("Successfully added " + req.query.GameName + '. Your Cart value including Tax is ₹' + (CartValue[User.Username] + 0.18*CartValue[User.Username]) + ".");
+          });
     }
-});
+  
+} 
 
-//order Using Params ;
-
-app.post('/order/:gameName', function(req, resp){
-    var gameName = req.params.gameName;
-    const name = gameName;
-    var price = gameDetail[gameName][0].Price;
-    if(gameDetail[gameName][3].Stock >= 1){
-        gameDetail[gameName][3].Stock -= 1;
-        var discountedprice = 0;
-        var ogPrice = price + 0.18*price;       //with Tax
-
-        //Some Offers
-        //if value price is greater than 1500, 10%off on total value
-        //if value price is greater than 2000, 15%off on total value
-
-        if(price >= 1500){
-            discountedprice = checkOffers(price).toString();
-            var discount = (15).toString();
-        }
-        else if(price >= 2000){
-            discountedprice = checkOffers(price).toString();
-            var discount = (20).toString();
-        }
-
-        if(discount){
-            resp.json("Congratulations!! Your have successfully ordered " + name + ". And you have earned a discount of " + discount + "%, your total amount with Tax is ₹" + ogPrice + ". With discount your total bill is ₹" +  discountedprice);
-        }
-        else{
-            resp.json("Congratulations!! Your have successfully ordered " + name + ".");
-        }
-    }
-
-    else{
-        resp.json("We're Sorry! " + gameName + " is currently not available. We have noted the product and will add it soon to the stock! Thank you!");
-    }
-});
-
-
-//Cart
-app.get('/cart', function(req, resp){
-    if(!status){
-        resp.json("Please log in first.");  //if the user is not logged in.
-    }
-    else{
-        if(User.Username in Cart && Cart[User.Username].length > 0){
-                resp.json(Cart[User.Username] + '.Your Cart Value including Tax is ₹' + (CartValue[User.Username] + 0.18*CartValue[User.Username])+ ".");
-        }
-        else{
-            resp.json("Your cart is empty."); 
-        }
-    }
-});
-
-
-//OrderCart
-app.get('/orderCart', function(req, resp){
-    if(!status){
-        resp.json("Please log in first.");  //if the user is not logged in.
-    }
-    else{
-        
-        if(User.Username in Cart){
-            const gameName = req.params.gameName;    
-            var price = CartValue[User.Username];
-            var discountedprice = 0;
-            var ogPrice = price + 0.18*price;       //with Tax
-
-            if(price >= 1500){
-                discountedprice = checkOffers(price).toString();
-                var discount = (15).toString();
-            }
-            else if(price >= 2000){
-                discountedprice = checkOffers(price).toString();
-                var discount = (20).toString();
-            }
-            if(discount){
-                Cart[User.Username] = [];
-                CartValue[User.Username] = 0;
-                resp.json("Congratulations!! Your Cart is successfully ordered. And you have earned a discount of " + discount + "%, your total amount with Tax is ₹" + ogPrice + ". With discount your total bill is ₹" +  discountedprice);
-            }
-            else{
-                resp.json("Congratulations!!  Your Cart is successfully ordered.");
-            }
-
-
-        }
-        else{
-            resp.json("Your cart is empty."); 
-        }
-    }
-});
-
-
-//Delete Cart Items
-app.delete('/delete', function(req, resp){
+function deleteFromBrand(gameName, creator){
+    var check = Brand[creator];
     var i = 0;
-    var check = Cart[User.Username];
-    var flag = false;
     for(i = 0; i < check.length; i++){
-        if(check[i] == req.query.productName){
-            //console.log(gameDetail[(req.query.productName)][0].Price);
-            CartValue[User.Username] -= gameDetail[(req.query.productName)][0].Price;
-            Cart[User.Username].splice(i, 1);
-            flag = true;
+        if(check[i] == gameName){
+            Brand[creator].splice(i, 1);
+            console.log("break");
             break;
         }
     }
-    if(!flag){
-        resp.json(req.query.productName + " is not in your cart yet.");
+}
+
+
+
+
+//CATEGORY
+
+app.get('/category', function(req, res){
+    var name = req.query;
+    if(Object.keys(name).length >= 1){
+        if(name.CategoryName in Category){
+            res.json(Category[name.CategoryName]);
+        }
+        else{
+            
+        }
     }
     else{
-        resp.json("Successfully deleted from your cart.");
+        res.json(Category);
     }
-
+    
 });
 
+
+//ADD CATEGORY
+
+app.post('/addcategory', function(req, res){
+    var category = req.query;
+
+    if(Object.keys(category).length == 1){
+        Category[category.CategoryName] = [];
+    }
+    else{
+        Category[category.CategoryName] = [req.query.ProductName];
+
+        //For Brand
+        brand = req.query.Creator;
+        var gameName = req.query.ProductName;
+        AddToBrand(brand, gameName)
+
+        //For GameDetail
+        if(!(gameName in gameDetail)){
+            AddToGameDetail(gameName, req.query.Price, req.query.Creator)
+        }
+    }
+    
+    res.json(Category);
+    
+});
+
+
+app.delete('/deleteCategory', function(req, res){
+    var name = req.query.GameName;
+    if(name in Category){
+        delete(Category[name]);
+        res.json(Category);
+    }
+    else{
+        res.json({});
+    }
+    
+});
+
+app.get('/getall', function(req, res){
+    res.json(gameDetail);
+});
+
+app.post('/productdescription', function(req, res){
+    var gameName = req.query.GameName ;
+    if(gameName in gameDetail){
+        res.json(gameDetail[gameName]);
+    }
+    else{
+        res.json({});
+    }
+    
+    
+});
+
+
+app.post('/addProduct', function(req, res){
+    gameName = req.query.ProductName;
+    creator = req.query.Creator;
+    category = req.query.Category;
+
+    //Updating Category
+    if(category in Category){
+        Category[category].push(gameName);
+    }
+    else{
+        Category[category] = [gameName];
+    }
+
+    //Updating Brand
+    if(creator in Brand){
+        Brand[creator].push(gameName);
+    }
+    else{
+        Brand[creator] = [gameName];
+    }
+    //Updating gamedetail
+    if(!(gameName in gameDetail)){
+        gameDetail[gameName] = [{'Price': req.query.Price}, {'Creator': creator}, {'Tax': 18}]
+    }
+    res.json(gameDetail);
+});
+
+app.put('/modifyproduct', function(req, res){
+    var gameName = req.query.productName;
+    var creator = req.query.Creator
+
+    //updating Creator 
+    if(gameDetail[gameName][1].Creator != creator){
+        var old = gameDetail[gameName][1].Creator;
+        gameDetail[gameName][1].Creator = creator;
+
+        deleteFromBrand(gameName, old);
+
+        if(creator in Brand){
+            Brand[creator].push(gameName);
+        }
+        else{
+            Brand[creator] = (gameName);
+
+        }
+        //updating Category
+        deleteFromCategory(gameName);
+        var newCat = req.query.CategoryName;
+        console.log(newCat);
+        if(newCat in Category){
+            Category[newCat].push(gameName);
+        }
+        else{
+            Category[newCat] = [gameName];
+        }
+
+        //updating Price
+        gameDetail[gameName][0].Price = req.query.Price ;
+    }
+    
+    res.json(gameDetail);
+
+
+    //updating 
+});
+
+app.delete('/deleteProduct', function(req, res){
+    var gameName = req.query.ProductName ;
+    console.log(gameDetail[gameName]);
+    var creator = gameDetail[gameName][1].Creator;
+    delete(gameDetail[gameName]);
+    //Deleting From Brand
+    deleteFromBrand(gameName, creator)
+    //Deleting from Category
+    deleteFromCategory(gameName);
+    res.json(gameDetail);
+});
+
+
+
+
+
+
+app.get('/brand', function(req, res){
+    var name = req.query;
+    if(Object.keys(name).length >= 1){
+        res.json(Brand[name.brandName]);
+    }
+    else{
+        res.json(Brand);
+    }
+});
+
+//Add Brand
+app.post('/addBrand', function(req, res){
+    var name = req.query;
+    
+    if(Object.keys(name).length == 1){
+        Brand[name.brandName] = [];
+    }
+    else{
+        var gameName = req.query.ProductName;
+        Brand[name.brandName] = [req.query.ProductName];
+    
+        //For GameDetail
+        if(!(gameName in gameDetail)){
+            AddToGameDetail(gameName, req.query.Price, req.query.brandName)
+        }
+
+        //For Category
+        var category = req.query.CategoryName;
+
+        if(category in Category){
+            Category[category].push(gameName);
+        }
+        else{
+            Category[category] = [gameName];
+        }
+    }
+    res.json(Brand);
+});
+
+app.delete('/deleteBrand', function(req, res){
+    var check = Brand[req.query.brandName];
+    delete(Brand[req.query.brandName]);
+    //Deleting from gameDetail
+    var i = 0;
+    for(i = 0; i < check.length; i++){
+        if(check[i] in gameDetail){
+            delete(gameDetail[check[i]]);
+        }
+    }
+    //Deleting from Category
+    deleteArrayFromCategory(check);
+    res.json(Brand);
+});
 
 app.listen(8080);
